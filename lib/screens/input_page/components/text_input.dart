b4f94c8b38de7e188c345ec6869b19daf5234b86
Hemish Pancholi <hemishpancholi11@gmail.com>
2021-02-06 13:59:41 +0530

    Created Input Page

---
+++ lib/screens/input_page/components/text_input.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TextInput extends StatelessWidget {
+  final String text;
+  final ValueChanged<String> onChanged;
+
+  const TextInput({Key? key, required this.text, required this.onChanged}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        hintText: text,
+      ),
+    );
+  }
+}
---
+++ lib/screens/input_page/input_page.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:vectorize/colors.dart';
+import 'package:vectorize/components/background.dart';
+import 'package:vectorize/components/glass_card.dart';
+import 'package:vectorize/screens/input_page/components/text_input.dart';
+import 'package:vectorize/screens/solution_page/solution_page.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Background(
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          Row(
+            children: [
+              SizedBox(width: size.width * 0.05),
+              GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: GlassCard(
+                  height: 80,
+                  width: 80,
+                  child: Center(
+                    child: Transform.translate(
+                      offset: Offset(8, 0),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 40,
+                        color: CustomColors.fontColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              GlassCard(
+                height: 80,
+                width: size.width * 0.82 - 80,
+                child: Center(
+                  child: Text(
+                    'Input Vector',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
+              SizedBox(width: size.width * 0.05),
+            ],
+          ),
+          const SizedBox(height: 30),
+          GlassCard(
+            height: 100,
+            width: size.width * 0.9,
+            child: Row(
+              children: [
+                const SizedBox(width: 30),
+                SizedBox(
+                  height: 70,
+                  width: 50,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: TextInput(
+                        text: 'x',
+                        onChanged: print,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  'î',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                ),
+                const Spacer(),
+                SizedBox(
+                  height: 70,
+                  width: 50,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: TextInput(
+                        text: 'y',
+                        onChanged: print,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  'î',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                ),
+                const Spacer(),
+                SizedBox(
+                  height: 70,
+                  width: 50,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: TextInput(
+                        text: 'z',
+                        onChanged: print,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  'î',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(width: 30),
+              ],
+            ),
+          ),
+          const SizedBox(height: 30),
+          GlassCard(
+            height: 80,
+            width: size.width * 0.9,
+            child: Center(
+              child: Text(
+                'Addition',
+                style: TextStyle(fontSize: 34, fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          GlassCard(
+            height: 100,
+            width: size.width * 0.9,
+            child: Row(
+              children: [
+                const SizedBox(width: 30),
+                SizedBox(
+                  height: 70,
+                  width: 50,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: TextInput(
+                        text: 'x',
+                        onChanged: print,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  'î',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                ),
+                const Spacer(),
+                SizedBox(
+                  height: 70,
+                  width: 50,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: TextInput(
+                        text: 'y',
+                        onChanged: print,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  'î',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                ),
+                const Spacer(),
+                SizedBox(
+                  height: 70,
+                  width: 50,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: TextInput(
+                        text: 'z',
+                        onChanged: print,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  'î',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(width: 30),
+              ],
+            ),
+          ),
+          const Spacer(),
+          GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SolutionPage(),
+              ),
+            ),
+            child: GlassCard(
+              height: 120,
+              width: size.width * 0.9,
+              child: Center(
+                child: Text(
+                  'Submit',
+                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.w700),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+        ],
+      ),
+    );
+  }
+}
